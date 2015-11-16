ae1793395fcbe54042b4304b7820aa586e569c0e
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-11-16 15:39:13 +0100

    Bara börjat

--- BettingApplication/BettingApplication/Controllers/HomeController.cs
+++ BettingApplication/BettingApplication/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace BettingApplication.Controllers
 {
     public class HomeController : Controller
     {
+
+        
         public ActionResult Index()
         {
             return View();
---
+++ BettingApplication/BettingApplication/Models/SoccerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BettingApplication.Scripts
+{
+  public class SoccerApi
+  {
+    public class SoccerModell
+    {
+      public string  { get; set; }
+    }
+  }
+}
