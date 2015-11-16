0db946e26dcd27c3e09d6d4e64aa66f0e467dddd
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-11-16 16:09:00 +0100

    Modell klar

--- BettingApplication/BettingApplication/Models/SoccerModel.cs
+++ BettingApplication/BettingApplication/Models/SoccerModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Xml.Linq;
 
 namespace BettingApplication.Scripts
 {
@@ -9,7 +11,35 @@ namespace BettingApplication.Scripts
   {
     public class SoccerModell
     {
-      public string  { get; set; }
+      public string homeTeamName { get; set; }
+      public string awayTeamName { get; set; }
+      public int result { get; set; }
+    }
+
+    public class SoccerRestService
+    {
+      private readonly string uri = "http://api.football-data.org/alpha/soccerseasons/356/fixtures";
+
+      public List<SoccerModell> GetSoccerApi()
+      {
+        var element = XDocument.Load(uri);
+        var json = new JavaScriptSerializer().Serialize(element);
+        var games = element.Descendants("").Descendants("")
+          .Select(el => new SoccerModell
+          {
+            homeTeamName = el.Element("homeTeamName").Value,
+            awayTeamName = el.Element("awayTeamName").Value,
+            result = int.Parse(el.Element("result").Value)
+          })
+          .ToList();
+
+        return games.Select(item => new SoccerModell
+        {
+          homeTeamName = item.homeTeamName,
+          awayTeamName = item.awayTeamName,
+          result = item.result
+        }).ToList();
+      }
     }
   }
 }
