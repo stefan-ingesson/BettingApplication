298382288067eb3ba8446b9e896be84e755d4513
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-11-17 13:16:09 +0100

    Ser datan från api, men kan inte visa

--- BettingApplication/BettingApplication/Models/LeagueTableREST.cs
+++ BettingApplication/BettingApplication/Models/LeagueTableREST.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace BettingApplication.Models
 {
     public class LeagueTableREST
     {
+
+        private readonly string uri = "http://brottsplatskartan.se/api.php?action=getEvents&period=1440";
+
+        public List<LeagueTable> GetLeagueTable()
+        {
+            XDocument element = XDocument.Load(uri);
+            var crimes = element.Descendants("events").Descendants("event")
+                .Select(el => new CrimeScene
+                {
+                    Description = el.Element("description").Value,
+                    Accuracy = int.Parse(el.Element("accuracy").Value),
+                    Place = el.Element("place").Value,
+                    Text = el.Element("text").Value
+                }
+                    )
+                    .ToList();
+
+            var scenes = new List<CrimeScene>();
+
+            foreach (var item in crimes)
+            {
+                var sceneOfCrime = new CrimeScene
+                {
+                    Description = item.Description,
+                    Accuracy = item.Accuracy,
+                    Place = item.Place,
+                    Text = item.Text
+                };
+                scenes.Add(sceneOfCrime);
+            }
+            return scenes;
+
+        }
     }
 }
