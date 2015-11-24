bc6b1d8452541fc285268f37ea54169293ac653b
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-11-24 14:11:58 +0100

    Nyfis

--- BettingApplication/BettingApplication/Controllers/HomeController.cs
+++ BettingApplication/BettingApplication/Controllers/HomeController.cs
@@ -85,6 +85,28 @@ namespace BettingApplication.Controllers
       return PartialView(fixtures.fixtures);
     }
 
+    public ViewResult ListTable()
+    {
+      var client = new HttpClient();
+      HttpRequestMessage table = new HttpRequestMessage
+      {
+        RequestUri = new Uri("http://api.football-data.org/alpha/soccerseasons/398/leagueTable"),
+        Method = HttpMethod.Get
+      };
+      #region Den här fungerar inte!
+      //table.Headers.Authorization = new AuthenticationHeaderValue("X-Auth-Token", "94212a25154c472696f2be2ee25e9691");
+      #endregion
+      table.Headers.Add("X-Auth-Token", "94212a25154c472696f2be2ee25e9691");
+      var response = client.SendAsync(table).Result;
+      if (response.StatusCode != HttpStatusCode.OK)
+      {
+        //return response.Content.ReadAsStringAsync().Result;
+        throw new ArgumentException();
+      }
+      LeagueTable leagueTable = JsonConvert.DeserializeObject<LeagueTable>(response.Content.ReadAsStringAsync().Result);
+      return View("ListTable", leagueTable.standing);
+    }
+
     public ActionResult PlaceBets()
     {
       return View();
